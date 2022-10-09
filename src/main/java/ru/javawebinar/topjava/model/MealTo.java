7a84381c648fcedebb8bcb624c4f0228e9ca7efa
sukhinlv <leva1981@yandex.ru>
2022-10-09 15:50:14 +0300

    HW1: change MealTo.id to int type and remove unused constructor

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -3,22 +3,18 @@ package ru.javawebinar.topjava.model;
 import java.time.LocalDateTime;
 
 public class MealTo {
-    private final Integer id;
+    private final int id;
     private final LocalDateTime dateTime;
     private final String description;
     private final int calories;
     private final boolean excess;
 
-    public MealTo(Integer id, LocalDateTime dateTime, String description, int calories, boolean excess) {
+    public MealTo(int id, LocalDateTime dateTime, String description, int calories, boolean excess) {
         this.id = id;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
         this.excess = excess;
-    }
-
-    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
-        this(0, dateTime, description, calories, excess);
     }
 
     @SuppressWarnings("unused")
