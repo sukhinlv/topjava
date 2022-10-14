5412a7f1779a44634eb358fe3530ad599dc29ec2
Leonid Sukhin <leva1981@yandex.ru>
2022-10-14 19:47:34 +0300

    HW2: Opt8: implement dummy logged user selection on index.html

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -9,12 +9,23 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import static org.slf4j.LoggerFactory.getLogger;
+import static ru.javawebinar.topjava.web.SecurityUtil.setAuthUserId;
 
 public class UserServlet extends HttpServlet {
     private static final Logger log = getLogger(UserServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+
+        if (action != null && action.equals("logUser")) {
+            int loggedUser = Integer.parseInt(request.getParameter("loggedUser"));
+            log.info("Log user with id={}", loggedUser);
+            setAuthUserId(loggedUser);
+            response.sendRedirect("meals");
+            return;
+        }
+
         log.debug("forward to users");
         request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
