719ce577edc7f4c70df5245ec877737ab9e0a5f8
Leonid Sukhin <leva1981@yandex.ru>
2022-10-16 21:44:04 +0300

    HW2: change storage structure of InMemoryUserRepository to Map<Integer, Map<Integer, Meal>> where first index is userId and second index is mealId

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -8,12 +8,14 @@ import ru.javawebinar.topjava.util.MealsUtil;
 import java.time.LocalDate;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryMealRepository implements MealRepository {
-    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private final Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
@@ -22,28 +24,46 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Meal save(Meal meal, int userId) {
-        if (meal.isNew()) {
-            meal.setId(counter.incrementAndGet());
-            meal.setUserId(userId);
-            repository.put(meal.getId(), meal);
-            return meal;
-        }
-        // handle case: update, but not present in storage
-        Meal updatedMeal = repository.computeIfPresent(meal.getId(),
-                (id, oldMeal) -> meal.getUserId() == userId && oldMeal.getUserId() == userId ? meal : oldMeal);
-        return (updatedMeal != null && updatedMeal.getUserId() == userId) ? updatedMeal : null;
+        AtomicReference<Meal> resultMeal = new AtomicReference<>(null);
+        repository.compute(userId, (unused, userMeals) -> {
+            if (userMeals == null) {
+                userMeals = new HashMap<>();
+            }
+            if (meal.isNew()) {
+                meal.setId(counter.incrementAndGet());
+                meal.setUserId(userId);
+                userMeals.put(meal.getId(), meal);
+                resultMeal.set(meal);
+            } else if (meal.getUserId() == userId) {
+                // handle case: update, but not present in storage
+                Meal updatedMeal = userMeals.computeIfPresent(meal.getId(),
+                        (id, oldMeal) -> oldMeal.getUserId() == userId ? meal : oldMeal);
+                resultMeal.set(updatedMeal != null && updatedMeal.getUserId() == userId ? updatedMeal : null);
+            }
+            return userMeals;
+        });
+        return resultMeal.get();
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        Meal meal = repository.get(id);
-        return meal == null || meal.getUserId() != userId ? false : repository.remove(id) != null;
+        AtomicBoolean result = new AtomicBoolean(false);
+        repository.computeIfPresent(userId, (unused, userMeals) -> {
+            result.set(userMeals.remove(id) != null);
+            return userMeals.size() == 0 ? null : userMeals;
+        });
+        return result.get();
     }
 
     @Override
     public Meal get(int id, int userId) {
-        Meal meal = repository.get(id);
-        return (meal != null && meal.getUserId() != userId) ? null : meal;
+        AtomicReference<Meal> resultMeal = new AtomicReference<>(null);
+        repository.computeIfPresent(userId, (unused, userMeals) -> {
+            Meal meal = userMeals.get(id);
+            resultMeal.set(meal != null && meal.getUserId() != userId ? null : meal);
+            return userMeals;
+        });
+        return resultMeal.get();
     }
 
     @Override
@@ -53,13 +73,18 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public List<Meal> getAllFiltered(int userId, LocalDate fromDate, LocalDate toDate) {
-        return repository.values().stream()
-                .filter(meal -> meal.getUserId() == userId)
-                .filter(meal -> fromDate == null || meal.getDate().compareTo(fromDate) >= 0)
-                .filter(meal -> toDate == null || meal.getDate().compareTo(toDate) <= 0)
-                .sorted(Comparator.comparing(Meal::getDate)
-                        .thenComparing(Meal::getTime)
-                        .reversed())
-                .collect(Collectors.toList());
+        AtomicReference<List<Meal>> resultMeal = new AtomicReference<>(null);
+        repository.computeIfPresent(userId, (unused, userMeals) -> {
+            resultMeal.set(userMeals.values().stream()
+                    .filter(meal -> meal.getUserId() == userId)
+                    .filter(meal -> fromDate == null || meal.getDate().compareTo(fromDate) >= 0)
+                    .filter(meal -> toDate == null || meal.getDate().compareTo(toDate) <= 0)
+                    .sorted(Comparator.comparing(Meal::getDate)
+                            .thenComparing(Meal::getTime)
+                            .reversed())
+                    .collect(Collectors.toList()));
+            return userMeals;
+        });
+        return resultMeal.get();
     }
 }
