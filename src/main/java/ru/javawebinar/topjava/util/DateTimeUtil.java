b40ab0713fa4d02e408969ccdf904381bb2b7403
Leonid Sukhin <leva1981@yandex.ru>
2022-10-17 18:42:51 +0300

    HW2: fix DateTimeUtils - correct using generics

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public static <T extends Comparable> boolean isBetweenHalfOpen(T checkedValue, T startValue, T endValue) {
+    public static <T extends Comparable<T>> boolean isBetweenHalfOpen(T checkedValue, T startValue, T endValue) {
         return (startValue == null || (checkedValue.compareTo(startValue) >= 0))
                 && (endValue == null || (checkedValue.compareTo(endValue) < 0));
     }
