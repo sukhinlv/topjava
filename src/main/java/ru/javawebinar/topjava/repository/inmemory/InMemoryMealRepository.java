b24e097dab73de3c2647277715e276739b3c47af
Leonid Sukhin <leva1981@yandex.ru>
2022-10-17 22:13:59 +0300

    HW2: fix InMemoryMealRepository.getAllFiltered now return NPE-safe result and refactor test data creation

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -7,10 +7,7 @@ import ru.javawebinar.topjava.util.DateTimeUtil;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.time.LocalDate;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -21,13 +18,8 @@ public class InMemoryMealRepository implements MealRepository {
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
-        // for test purposes half of meals will be created with userId = 1, and other half with userId = 2
-        Iterator<Meal> mealsIterator = MealsUtil.meals.iterator();
-        int counter = 0;
-        while (mealsIterator.hasNext()) {
-            Meal meal = mealsIterator.next();
-            this.save(meal, counter++ < 7 ? 1 : 2);
-        }
+        MealsUtil.meals1.forEach(meal -> this.save(meal, 1));
+        MealsUtil.meals2.forEach(meal -> this.save(meal, 2));
     }
 
     @Override
@@ -63,7 +55,7 @@ public class InMemoryMealRepository implements MealRepository {
     @Override
     public List<Meal> getAllFiltered(int userId, LocalDate fromDate, LocalDate toDate) {
         Map<Integer, Meal> userMeals = repository.get(userId);
-        return userMeals == null ? null : userMeals.values().stream()
+        return userMeals == null ? new ArrayList<>() : userMeals.values().stream()
                 .filter(meal -> DateTimeUtil.isBetweenHalfOpen(meal.getDate(), fromDate, toDate == null ? null : toDate.plusDays(1)))
                 .sorted(Comparator.comparing(Meal::getDateTime).reversed())
                 .collect(Collectors.toList());
