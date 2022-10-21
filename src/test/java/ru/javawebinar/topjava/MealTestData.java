dd6c90bf5987b3acfe6bfbde0bd85756d87c8ef2
Leonid Sukhin <leva1981@yandex.ru>
2022-10-21 17:52:17 +0300

    HW3: fix MealTestData.assertMatch() used isEqual() without field-by-field comparison

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -50,6 +50,6 @@ public class MealTestData {
     }
 
     public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
     }
 }
