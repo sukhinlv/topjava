c31f59a643a569984a4c02a4571c8c46febfa6af
Leonid Sukhin <leva1981@yandex.ru>
2022-10-21 17:09:48 +0300

    HW3: refactor initDB.sql

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -28,12 +28,11 @@ CREATE TABLE user_roles
 CREATE TABLE meals
 (
     id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-    date_time         TIMESTAMP           DEFAULT now() NOT NULL,
+    date_time         TIMESTAMP                        NOT NULL,
     description      VARCHAR                           NOT NULL,
     calories         INTEGER                           NOT NULL,
     user_id          INTEGER                           NOT NULL,
     CONSTRAINT meals_userid_datetime_idx UNIQUE (user_id, date_time),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
-CREATE UNIQUE INDEX meals_unique_userid_datetime ON meals (user_id, date_time);
-CREATE UNIQUE INDEX meals_unique_userid_id ON meals (user_id, id);
+CREATE INDEX meals_unique_userid_id_idx ON meals (user_id, id);
