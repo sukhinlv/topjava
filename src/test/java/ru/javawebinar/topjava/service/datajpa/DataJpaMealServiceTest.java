68a15d8968c570e4e3efe555c9bf0988f5e52a00
Leonid Sukhin <leva1981@yandex.ru>
2022-11-06 13:43:42 +0300

    HW5: add getByIdWithUserNotFound and getByIdWithUserNotOwn to test getByIdWithUser() when meal is not found and meal is not owned by user

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -5,10 +5,13 @@ import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @ActiveProfiles(profiles = Profiles.DATAJPA)
 public class DataJpaMealServiceTest extends AbstractMealServiceTest {
@@ -20,5 +23,15 @@ public class DataJpaMealServiceTest extends AbstractMealServiceTest {
         assertThat(actual.getUser())
                 .usingRecursiveComparison()
                 .ignoringFields("password", "meals").isEqualTo(admin);
+    }
+
+    @Test
+    public void getByIdWithUserNotFound() {
+        assertThrows(NotFoundException.class, () -> service.getByIdWithUser(NOT_FOUND, USER_ID));
+    }
+
+    @Test
+    public void getByIdWithUserNotOwn() {
+        assertThrows(NotFoundException.class, () -> service.getByIdWithUser(ADMIN_MEAL_ID, USER_ID));
     }
 }
