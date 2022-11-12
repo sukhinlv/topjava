59b83102bedd9ab8fbc9fc18c42734be54ae3fba
Leonid Sukhin <leva1981@yandex.ru>
2022-11-12 16:36:05 +0300

    HW6: fix all JdbcUserRepository methods (and tests) for use with multi-roles users

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -2,35 +2,37 @@ package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 @Repository
 @Transactional(readOnly = true)
 public class JdbcUserRepository implements UserRepository {
 
     private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
-
     private final JdbcTemplate jdbcTemplate;
-
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-
     private final SimpleJdbcInsert insertUser;
 
     @Autowired
     public JdbcUserRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-        this.insertUser = new SimpleJdbcInsert(jdbcTemplate)
-                .withTableName("users")
-                .usingGeneratedKeyColumns("id");
+        this.insertUser = new SimpleJdbcInsert(jdbcTemplate).withTableName("users").usingGeneratedKeyColumns("id");
 
         this.jdbcTemplate = jdbcTemplate;
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
@@ -44,13 +46,37 @@ public class JdbcUserRepository implements UserRepository {
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(parameterSource);
             user.setId(newKey.intValue());
+            saveUserRoles(user, user.id());
         } else if (namedParameterJdbcTemplate.update("""
-                   UPDATE users SET name=:name, email=:email, password=:password, 
+                   UPDATE users SET name=:name, email=:email, password=:password,
                    registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id
                 """, parameterSource) == 0) {
             return null;
+        } else {
+            jdbcTemplate.update("DELETE FROM user_roles WHERE user_id = ?", user.id());
+            saveUserRoles(user, user.id());
         }
         return user;
+    }
+
+    private void saveUserRoles(User user, int newId) {
+        Set<Role> roles = user.getRoles();
+        if (!roles.isEmpty()) {
+            jdbcTemplate.batchUpdate("INSERT INTO user_roles (user_id, role) VALUES (?,?)", new BatchPreparedStatementSetter() {
+                @Override
+                public void setValues(PreparedStatement ps, int i) throws SQLException {
+                    for (Role role : roles) {
+                        ps.setInt(1, newId);
+                        ps.setString(2, role.name());
+                    }
+                }
+
+                @Override
+                public int getBatchSize() {
+                    return roles.size();
+                }
+            });
+        }
     }
 
     @Override
@@ -61,19 +87,52 @@ public class JdbcUserRepository implements UserRepository {
 
     @Override
     public User get(int id) {
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
-        return DataAccessUtils.singleResult(users);
+        return queryRolesForUser(jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id));
+    }
+
+    private User queryRolesForUser(List<User> users) {
+        if (users == null || users.size() == 0) {
+            return null;
+        }
+        User user = DataAccessUtils.singleResult(users);
+        List<String> roles = jdbcTemplate.queryForList("SELECT role FROM user_roles WHERE user_id=?", String.class, user.id());
+        user.setRoles(roles.stream().map(Role::valueOf).toList());
+        return user;
     }
 
     @Override
     public User getByEmail(String email) {
-//        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        return DataAccessUtils.singleResult(users);
+        return queryRolesForUser(jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email));
     }
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        List<User> users = jdbcTemplate.query("SELECT us.*, ur.role FROM users AS us " + "LEFT JOIN user_roles AS ur ON us.id = ur.user_id " + "ORDER BY name, email", new UserWithRolesRowMapper());
+        return users.stream().distinct().toList();
+    }
+
+    private static class UserWithRolesRowMapper implements RowMapper<User> {
+
+        private final Map<Integer, User> usersMap = new HashMap<>();
+
+        @Override
+        public User mapRow(@Nullable ResultSet rs, int rowNum) throws SQLException {
+            Objects.requireNonNull(rs);
+            User user = Objects.requireNonNull(ROW_MAPPER.mapRow(rs, rowNum));
+
+            String roleValue = rs.getString("role");
+            if (roleValue != null && !roleValue.isEmpty()) {
+                user.setRoles(List.of(Role.valueOf(roleValue)));
+            } else {
+                user.setRoles(List.of());
+            }
+
+            user = usersMap.merge(user.id(), user, (oldUser, newUser) -> {
+                newUser.getRoles().stream().findFirst().ifPresent(newRole -> oldUser.getRoles().add(newRole));
+                return oldUser;
+            });
+
+            return user;
+        }
     }
 }
