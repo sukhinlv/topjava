f97124db506a29befd24ec31d5132f51f13a1b06
Leonid Sukhin <leva1981@yandex.ru>
2022-11-14 16:39:06 +0300

    HW6: change logger implementation in AbstractMealController

--- src/main/java/ru/javawebinar/topjava/web/meal/AbstractMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/AbstractMealController.java
@@ -17,7 +17,7 @@ import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
 public abstract class AbstractMealController {
-    private static final Logger log = LoggerFactory.getLogger(AbstractMealController.class);
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     private final MealService service;
 
