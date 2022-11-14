fbb11197e3a97313cc0f13752b70510928e3292c
Leonid Sukhin <leva1981@yandex.ru>
2022-11-14 17:47:42 +0300

    HW6: refactor RootController

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +15,8 @@ import javax.servlet.http.HttpServletRequest;
 public class RootController {
     private static final Logger log = LoggerFactory.getLogger(RootController.class);
 
-    private final UserService service;
-
-    public RootController(UserService service) {
-        this.service = service;
-    }
+    @Autowired
+    private UserService service;
 
     @GetMapping("/")
     public String root() {
