98edb60e2b0d20a67c08b91907ac25c3a9892cf1
Leonid Sukhin <leva1981@yandex.ru>
2022-11-16 20:46:36 +0300

    HW6: refactor AbstractJpaUserServiceTest

--- src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
@@ -1,22 +1,4 @@
 package ru.javawebinar.topjava.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import ru.javawebinar.topjava.repository.JpaUtil;
-
-import java.util.Arrays;
-
-import static ru.javawebinar.topjava.Profiles.NO_CACHE;
-
 public abstract class AbstractJpaUserServiceTest extends AbstractUserServiceTest {
-
-    @Autowired
-    protected JpaUtil jpaUtil;
-
-    @Override
-    public void setup() {
-        super.setup();
-        if (!Arrays.asList(env.getActiveProfiles()).contains(NO_CACHE)) {
-            jpaUtil.clear2ndLevelHibernateCache();
-        }
-    }
 }
