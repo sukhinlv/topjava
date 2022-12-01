2fb735022af97d71c17b1902d300ef1f98e171b2
Leonid Sukhin <leva1981@yandex.ru>
2022-12-01 17:55:22 +0300

    HW8: first commit

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,43 @@
+const mealAjaxUrl = "meals/";
+
+// https://stackoverflow.com/a/5064235/548473
+const ctx = {
+    ajaxUrl: mealAjaxUrl
+};
+
+// $(document).ready(function () {
+$(function () {
+    $('#datetimepicker').datetimepicker();
+
+    makeEditable(
+        $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "date/time"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "desc"
+                ]
+            ]
+        })
+    );
+});
