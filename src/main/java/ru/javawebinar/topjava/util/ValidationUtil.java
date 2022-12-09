7efe734ea60f13efe6f5c235df9ab2961ac814e5
Leonid Sukhin <leva1981@yandex.ru>
2022-12-09 11:53:16 +0300

    HW9: change ValidationUtil#processBindingErrors to process isBindingFailure()

--- src/main/java/ru/javawebinar/topjava/util/ValidationUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/ValidationUtil.java
@@ -78,7 +78,9 @@ public class ValidationUtil {
 
     public static ResponseEntity<String> processBindingErrors(BindingResult result) {
         String errorFieldsMsg = result.getFieldErrors().stream()
-                .map(fe -> String.format("[%s] %s", fe.getField(), fe.getDefaultMessage()))
+                .map(fe -> fe.isBindingFailure() ?
+                        String.format("[%s] incorrect value", fe.getField()) :
+                        String.format("[%s] %s", fe.getField(), fe.getDefaultMessage()))
                 .collect(Collectors.joining("<br>"));
         return ResponseEntity.unprocessableEntity().body(errorFieldsMsg);
     }
